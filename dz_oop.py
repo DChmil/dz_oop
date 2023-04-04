0121341d660903edfecf4115057c5a86379b5ff9
DmAChmil <dmitriy-900@yandex.ru>
2023-04-05 02:02:09 +0300

    Commit part 3

--- dz_oop.py
+++ dz_oop.py
@@ -16,6 +16,33 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        sn = []
+        if self.finished_courses == []:
+            sn = ['Введение в программирование']
+        else:
+            sn.extend(self.finished_courses)
+        st_grate = self.summ()
+        separat = ", "
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {st_grate}\nКурсы в процессе изучения: {separat.join(self.courses_in_progress)}\nЗавершенные курсы: {separat.join(sn)}\n'
+        return res
+
+    def summ(self):
+        st = []
+        for lect in self.grades:
+            st.extend(self.grades[lect])
+        if st == []:
+            st_grate = 'Нет оценок'
+        else:
+            st_grate = round(sum(st) / len(st), 2)
+        return st_grate
+
+    def __lt__(self, other):
+        if not isinstance(other, Student):
+            print('ERROR')
+            return
+        return self.summ() < other.summ()
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -27,6 +54,27 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        st_grate = self.summ()
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка: {st_grate}\n'
+        return res
+
+    def summ(self):
+        st = []
+        for lect in self.grades:
+            st.extend(self.grades[lect])
+        if st == []:
+            st_grate = 'Нет оценок'
+        else:
+            st_grate = round(sum(st) / len(st), 2)
+        return st_grate
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('ERROR')
+            return
+        return self.summ() < other.summ()
+
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -36,6 +84,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\n'
+        return res
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
